b3058b451c54ed7d5abb33c2d795ebd2216faf18
Its-me-Andreeei <andrei.karolyi@student.upt.ro>
2023-05-21 19:39:28 +0300

    [CODE] Bug fix for comparison of password
    [CODE] Bug fix for comparison of password

--- SourceFiles/Master/Master.ino
+++ SourceFiles/Master/Master.ino
@@ -1,37 +1,5 @@
-#include <AES.h>
-#include <AuthenticatedCipher.h>
-#include <BLAKE2b.h>
-#include <BLAKE2s.h>
-#include <BigNumberUtil.h>
-#include <BlockCipher.h>
-#include <CTR.h>
-#include <ChaCha.h>
-#include <ChaChaPoly.h>
-#include <Cipher.h>
 #include <Crypto.h>
-#include <Curve25519.h>
-#include <EAX.h>
-#include <Ed25519.h>
-#include <GCM.h>
-#include <GF128.h>
-#include <GHASH.h>
-#include <HKDF.h>
-#include <Hash.h>
-#include <KeccakCore.h>
-#include <NoiseSource.h>
-#include <OMAC.h>
-#include <P521.h>
-#include <Poly1305.h>
-#include <RNG.h>
-#include <SHA224.h>
 #include <SHA256.h>
-#include <SHA3.h>
-#include <SHA384.h>
-#include <SHA512.h>
-#include <SHAKE.h>
-#include <XOF.h>
-#include <XTS.h>
-#include <avr/sleep.h>
 
 #include <Wire.h>
 #include <LiquidCrystal.h>
@@ -156,7 +124,7 @@ void loop(){
     boolean pass_OK = true;
     for(byte i = 0; i<sizeof(password_encrypted); i++)
     {
-      if(pass_to_be_checked_after_encryption[i] != password_encrypted)
+      if(pass_to_be_checked_after_encryption[i] != password_encrypted[i])
         pass_OK = false;
     }
     if(pass_OK)
